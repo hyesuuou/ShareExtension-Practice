063640111011292816f0833d7a1b1794a1d6881d
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-07-08 01:23:15 +0900

    주석추가

--- Share-Extension/Share-Extension/ViewController.swift
+++ Share-Extension/Share-Extension/ViewController.swift
@@ -15,13 +15,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        
+        /// Share Extension과 원래 앱을 같은 App Group을 이용해 같은 그룹으로 (group.kimhyesu.Share) 묶는다.
+        /// 그리고 해당 그룹의 유저디폴츠에 앱에 저장된 토큰을 세팅해주고, 동기화시켜주면
+        /// 쉐어익스텐션에서 해당 내용을 전달받을 수 있다.
         let defaults = UserDefaults(suiteName: "group.kimhyesu.Share")
         defaults?.set(APITokens.token, forKey: "token")
         defaults?.synchronize()
     }
     
+    /// share sheet의 textfield에 입력하고 post 했던 내용을
+    /// 앱 내부로 돌아와 text 버튼을 누르면 보이게 한다.
     @IBAction func textButtonDidTap(_ sender: Any) {
         print(UserDefaults(suiteName: "group.kimhyesu.Share")?.string(forKey: "text") ?? "")
     }
--- Share-Extension/Share/ShareViewController.swift
+++ Share-Extension/Share/ShareViewController.swift
@@ -16,11 +16,13 @@ class ShareViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /// 앱에서 전달받은 토큰을 라벨에 표시한다.
         tokenLabel.text = UserDefaults(suiteName: "group.kimhyesu.Share")?.string(forKey: "token")
     }
 
     @IBAction func postButtonDidTap(_ sender: Any) {
         
+        /// UserDefaults에 Textfield에 담긴 내용을 저장한다. (이후 앱에서 가져올 수 있도록)
         let defaults = UserDefaults(suiteName: "group.kimhyesu.Share")
         defaults?.set(textField.text ?? "", forKey: "text")
         defaults?.synchronize()
